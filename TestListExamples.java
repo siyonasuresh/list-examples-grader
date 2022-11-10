c4df6abf6b170dac458f57a4e4dbe07524a67244
BenHinnant <55713184+BenHinnant@users.noreply.github.com>
2022-11-10 15:43:43 -0800

    start

--- TestListExamples.java
+++ TestListExamples.java
@@ -3,4 +3,9 @@ import org.junit.*;
 
 public class TestListExamples {
   // Write your grading tests here!
+  @Test 
+  public void testSumEvensLength4() {
+    int[] input1 = { 12, 13, 7, 2};
+    assertEquals(EvensExample.sumEvenIndices(input1), 19);
+  }
 }
--- grade.sh
+++ grade.sh
@@ -1,7 +1,37 @@
-# Create your grading script here
-
 set -e
-
+ 
 rm -rf student-submission
 git clone $1 student-submission
-
+cd student-submission
+ 
+FILE=./ListExamples.java
+if test -f "$FILE" ;
+then
+    echo "$FILE exists."
+else
+    echo "$FILE does not exists or is not a regular file."
+fi
+ 
+mkdir -p ./testdir
+cp ./ListExamples.java ./testdir
+cp ../TestListExamples.java ./testdir
+ 
+cd ./testdir
+set +e
+javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" ListExamples.java
+if [ $? -eq 0 ]
+then
+    echo "Compiled Successfully. :)"
+else
+    echo "Did not Compile Successfully. :("
+fi
+ 
+java ListExamples
+if [ $? -eq 0 ]
+then
+    echo "Java Ran Successfully. :)"
+else
+    echo "Java did not run Successfully. :("
+fi
+ 
+ 
