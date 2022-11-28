53d435296c15f05c018f525f012d3cedf6aafd51
siyonasuresh <114272611+siyonasuresh@users.noreply.github.com>
2022-11-28 10:20:58 -0800

    lab report 5

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,30 +1,32 @@
 import static org.junit.Assert.*;
 import org.junit.*;
-
+import java.util.ArrayList;
 public class TestListExamples {
   // Write your grading tests here!
-  @Test 
-	public void testFilter1() {
-        ArrayList<String> input1 = new ArrayList<>();
-        StringChecker sc = new StringChecker();
-        input1.add(0, "apple");
-        input1.add(1, "banana");
-        input1.add(2, "citrus");
-        assertEquals(input1, ListExamples.filter(input1, sc));
+  class IfLengthLargerThanTwo implements StringChecker{
+    public boolean checkString(String s){
+      if(s.length() > 2){
+        return true;
+      }
+      return false;
     }
-  @Test 
-	public void testFilter2() {
-        ArrayList<String> input1 = new ArrayList<>();
-        StringChecker sc = new StringChecker();
-        input1.add(0, "apple");
-        input1.add(2, "citrus");
-        assertEquals(input1, ListExamples.filter(input1, sc));
+  }
+  @Test(timeout = 100)
+    public void testFilter(){
+        ArrayList<String> arr = new ArrayList<>(); arr.add("a"); arr.add("aa"); arr.add("aaa"); arr.add("aaaa");
+        ArrayList<String> arr2 = new ArrayList<>(); arr2.add("aaa"); arr2.add("aaaa");
+
+        assertEquals(arr2, ListExamples.filter(arr, new IfLengthLargerThanTwo()));
+        
     }
-  @Test 
-	public void testFilter3() {
-        ArrayList<String> input1 = new ArrayList<>();
-        StringChecker sc = new StringChecker();
-        input1.add(0, "apple");
-        assertEquals(input1, ListExamples.filter(input1, sc));
+
+    @Test(timeout = 100)
+    public void testMerge(){
+        ArrayList<String> arr = new ArrayList<>(); arr.add("a"); arr.add("c"); 
+        ArrayList<String> arr2 = new ArrayList<>(); arr2.add("b"); arr2.add("d"); arr2.add("e"); arr2.add("f");
+        ArrayList<String> arr3 = new ArrayList<>(); arr3.add("a"); arr3.add("b"); arr3.add("c"); arr3.add("d");
+         arr3.add("e"); arr3.add("f");
+
+        assertEquals(arr3, ListExamples.merge(arr, arr2));
     }
 }
--- grade.sh
+++ grade.sh
@@ -1,37 +1,46 @@
 set -e
- 
+
 rm -rf student-submission
+mkdir student-submission
 git clone $1 student-submission
+
+cp TestListExamples.java student-submission
 cd student-submission
- 
-FILE=./ListExamples.java
-if test -f "$FILE" ;
-then
-    echo "$FILE exists."
+
+CPATH=.:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar
+
+if [[ -f ListExamples.java ]];
+then 
+    echo "ListExamples.java file found!"
 else
-    echo "$FILE does not exists or is not a regular file."
+    echo "File Not Found! Make sure to submit the 'ListExamples.java file to be graded!"
+    echo "Grade: Fail"
+    exit 1
 fi
- 
-mkdir -p ./testdir
-cp ./ListExamples.java ./testdir
-cp ../TestListExamples.java ./testdir
- 
-cd ./testdir
+
 set +e
-javac -cp ".;../../lib/hamcrest-core-1.3.jar;../../lib/junit-4.13.2.jar" *.java
-if [ $? -eq 0 ]
+
+javac -cp $CPATH *.java
+
+if [[ $? -eq 0 ]]
 then
-    echo "Compiled Successfully. :)"
+    echo "Files have been compiled successfully!"
 else
-    echo "Did not Compile Successfully. :("
+    echo "Error when compiling files!!"
+    echo "Grade: Fail"
+    exit 2
 fi
- 
-java TestListExamples
-if [ $? -eq 0 ]
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > results.txt 
+
+if [[ $? -eq 0 ]]
 then
-    echo "Java Ran Successfully. :)"
+    echo "All Tests Passed!"
+    echo "Grade: Pass"
+    exit
 else
-    echo "Java did not run Successfully. :("
+    cat results.txt
+    echo "are wrong!"
+    echo "Grade: Fail"
+    exit 3
 fi
- 
- 
